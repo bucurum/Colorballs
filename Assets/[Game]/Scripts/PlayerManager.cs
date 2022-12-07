ba00c775058898aabc0f2f4d44c1588006c16c5e
bucurum <bucurum18@posta.pau.edu.tr>
2022-12-07 21:51:16 +0300

    making level desing, added player jump move, adjust camera y axsis

--- Assets/[Game]/Scripts/PlayerManager.cs
+++ Assets/[Game]/Scripts/PlayerManager.cs
@@ -9,16 +9,22 @@ public class PlayerManager : MonoBehaviour
     private bool moveTheBall;
     [Range(0f, 1f)] [SerializeField] float maxSpeed;
     [Range(0f, 1f)] [SerializeField] float camSpeed;
-    [Range(0f, 20f)] [SerializeField] float pathSpeed;
+    [Range(0f, 60f)] [SerializeField] float pathSpeed;
     private float velocity;
-    private float camVelocity;
+    private float camVelocityX;
+    private float camVelocityY;
     private Camera mainCamera;
     public Transform path; 
+    public Rigidbody rb;
+    public Collider coll;
+
 
     void Start()
     {
         ball = transform;
         mainCamera = Camera.main;
+        rb = GetComponent<Rigidbody>();
+        coll = GetComponent<Collider>();
     }
 
     void Update()
@@ -67,9 +73,13 @@ public class PlayerManager : MonoBehaviour
 
     void LateUpdate()
     {
-        var CameraNewPos = mainCamera.transform.position;
-        mainCamera.transform.position = new Vector3(Mathf.SmoothDamp(CameraNewPos.x, ball.transform.position.x, ref camVelocity, camSpeed)
-        , CameraNewPos.y, CameraNewPos.z);
+        if (rb.isKinematic)
+        {
+            var CameraNewPos = mainCamera.transform.position;
+            mainCamera.transform.position = new Vector3(Mathf.SmoothDamp(CameraNewPos.x, ball.transform.position.x, ref camVelocityX, camSpeed)
+            , Mathf.SmoothDamp(CameraNewPos.y, ball.transform.position.y + 3f, ref camVelocityY, camSpeed), CameraNewPos.z);
+  
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -78,6 +88,26 @@ public class PlayerManager : MonoBehaviour
         {
             gameObject.SetActive(false);
             MenuManager.MenuManagerInstance.GameState = false;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("path"))
+        {
+            rb.isKinematic = false;
+            coll.isTrigger = false;
+            rb.velocity = new Vector3(0f, 8f, 0f);
+            pathSpeed = pathSpeed *2 ;
+
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("path"))
+        {
+            rb.isKinematic = true;
+            coll.isTrigger = true;
+            pathSpeed = pathSpeed / 2;
         }
     }
 }
